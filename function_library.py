d3415fd7d5acdc9e2b64af3f2486792f5347a9e2
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2024-03-10 10:15:08 +0100

    update

--- function_library.py
+++ function_library.py
@@ -1,10 +1,6 @@
 import matplotlib.pyplot as plt
 
 '''
-to fix: weird bug with the bishop and illegal moves
-'''
-
-'''
 white:
 ♔
 ♕
@@ -21,12 +17,12 @@ black
 ♟︎
 '''
 
-#piece_shorts = ['rw','nw','bw','qw','kw','bw','nw','rw','pw','pw','pw','pw','pw','pw','pw','pw','rb','nb','bb','qb','kb','bb','nb','rb','pb','pb','pb','pb','pb','pb','pb','pb']
+piece_shorts = ['rw','nw','bw','qw','kw','bw','nw','rw','pw','pw','pw','pw','pw','pw','pw','pw','rb','nb','bb','qb','kb','bb','nb','rb','pb','pb','pb','pb','pb','pb','pb','pb']
 
-#piece_starting_positions =['a1','b1','c1','d1','e1','f1','g1','h1','a2','b2','c2','d2','e2','f2','g2','h2','a8','b8','c8','d8','e8','f8','g8','h8','a7','b7','c7','d7','e7','f7','g7','h7']
+piece_starting_positions =['a1','b1','c1','d1','e1','f1','g1','h1','a2','b2','c2','d2','e2','f2','g2','h2','a8','b8','c8','d8','e8','f8','g8','h8','a7','b7','c7','d7','e7','f7','g7','h7']
 
-piece_shorts = ['bw','qb','qw']
-piece_starting_positions = ['c1','d2','b2']
+#piece_shorts = ['bw','qb','qw']
+#piece_starting_positions = ['c1','d2','b2']
 
 
 
